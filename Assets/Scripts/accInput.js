e75e64ad3c6e80db3a95f119aece4f015a8bc613
Ted <theted@berkeley.edu>
2014-11-22 02:32:14 -0800

    pls work

---
+++ Assets/Scripts/accInput.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+
+
+var why : Vector3;
+var toMove : Vector3;
+
+function Start () {
+
+}
+
+
+
+function FixedUpdate () {
+	var forceFactorX : int = 18;
+	var forceFactorY : int = 25;
+	
+	
+
+	toMove = Vector3.zero;
+	toMove.x = Input.acceleration.x;
+	toMove.z = Input.acceleration.y;
+	toMove.x *= forceFactorX;
+	toMove.z *= forceFactorY;
+	
+	rigidbody.AddRelativeForce(toMove);
+}
+
+
+function OnGUI () {
+	GUI.Box (Rect (10,10,100,90), toMove.ToString());
+}
+
